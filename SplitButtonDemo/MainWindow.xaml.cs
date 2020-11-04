8d93d58905ea16f6555f0ed6e962ef806eadb7aa
Peter Nowak <peter.nowak@ecosio.com>
2020-11-04 18:39:42 +0100

    Add project files.

---
+++ SplitButtonDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SplitButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public List<Tuple<string, int>> SampleData
+        {
+            get
+            {
+                return new List<Tuple<string, int>>
+                {
+                    new Tuple<string, int>("Test", 0),
+                    new Tuple<string, int>("Another Test", 1)
+                };
+            }
+        }
+    }
+}
